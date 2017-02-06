2f01fd00de54ecddbd06446b81f7da8f51eda2d6
ADEsposito <a.desposito640@gmail.com>
2017-02-06 17:46:02 -0600

    refactor length function

---
+++ app.js
@@ -0,0 +1,12 @@
+function lengths(arrayOfStrings) {
+  wordCount = [];
+  for(i = 0; i < arrayOfStrings.length; i++) {
+    var word = arrayOfStrings[i];
+    wordCount.push(word.length);
+  }
+  return wordCount;
+}
+
+function transmogrifier(x, y, z) {
+
+}
